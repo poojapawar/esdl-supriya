619c93391b5da088172502621541d823a9ff38bf
poojapawar <pooja.m.pawar@cumminscollege.in>
2014-07-31 10:51:58 +0530

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -3,8 +3,15 @@ void main()
 {
   cout<<"Welcome to github";
   cout<<"Welcome to cummins college";
+  cout<<"square"<<square(2);
+  cout<<"cube"<<cube(3);
 }
 double square(double in_number)
 {
   return in_number*in_number;
+}
+
+double cube(double in_number)
+{
+  return in_number*in_number*in_number;
 }
